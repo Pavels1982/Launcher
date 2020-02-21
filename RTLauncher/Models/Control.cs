84df8801bf45723aee83aaa701d9f8fb82ccf8f6
pavels1982@mail.ru <pavels1982@mail.ru>
2020-02-21 17:39:18 +0300

    first commit

---
+++ RTLauncher/Models/Control.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RTLauncher.Models
+{
+    public class Control
+    {
+        public object Content { get; set; }
+        public object Icon { get; set; }
+        public string Title { get; set; }
+        
+
+        public Control(object view, object viewModel, object icon, string title)
+        {
+            this.Content = view;
+            (this.Content as UserControl).DataContext = viewModel;
+            this.Icon = icon;
+            this.Title = title;
+        }
+    }
+}
---
+++ RTLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using RTLauncher.CustomControls;
+using RTLauncher.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RTLauncher.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Control[] Controls;
+      
+        public ObservableCollection<Control> ControlList { get; set; } = new ObservableCollection<Control>();
+
+        public Control SelectedControl { get; set; }
+      
+        public bool IsMenuOpen { get; set; }
+
+        public ICommand MenuAction
+        {
+            get
+            {
+                return new RelayCommand((o) => 
+                {
+                    IsMenuOpen = !IsMenuOpen;
+                });
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            CreateControls();
+            SelectedControl = Controls.Where(c => c.Title == "Home").FirstOrDefault() as Control;
+        }
+
+
+        public void CreateControls()
+        {
+            Controls = new Control[] {
+
+                new Control(
+                    new HomeControl(),
+                    new HomeControlViewModel(),
+                    null,
+                    "Home"),
+
+               new Control(
+                    new SettingControl(),
+                    new SettingControlViewModel(),
+                    null,
+                    "Settings"),
+
+            };
+        }
+
+    }
+}
